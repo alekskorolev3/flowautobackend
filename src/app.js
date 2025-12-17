2781fd3450492c37d3a7c66a7f64ec777bbfb1ec
alekskorolev3 <alekskorolev3@gmail.com>
2025-12-18 00:43:06 +0300

    Added CORS origin

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import requestsRoutes from './routes/requests.routes.js'
 
 const app = express()
+
+app.use(cors({
+    origin: ['https://flowauto.ru'], // разрешенные домены
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true // если нужны куки
+}))
 
 app.use(express.json())
 
