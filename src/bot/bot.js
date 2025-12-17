bc7c8faf3f8b938628ec430f8cb7fd4588fe9da8
alekskorolev3 <alekskorolev3@gmail.com>
2025-12-18 00:26:46 +0300

    Fixed telegram bot connection 222

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,5 +1,9 @@
 import dotenv from 'dotenv'
-dotenv.config({ path: '../.env' })
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
 import { Telegraf } from 'telegraf'
 import { adminMiddleware } from './middlewares/admin.js'
